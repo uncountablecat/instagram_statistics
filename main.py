85840d3eaee5d7dacb318c127318db87aa9e3d26
Chao <chaoguo@Chaos-MacBook-Air.local>
2016-08-09 11:44:10 -0700

    restructured tables

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import time
 import urllib as urllib
 import requests
 import unicodecsv as csv # emoji might be in captions
@@ -40,11 +41,11 @@ def extract_statistics(post_list):
 	caption_list = []
 
 	for i in range(0, num_of_posts):
-		stats_list.append([post_list[i]['code'], post_list[i]['likes']['count'], post_list[i]['comments']['count']])
+		stats_list.append([insta_id,post_list[i]['code'], post_list[i]['likes']['count'], post_list[i]['comments']['count'],int(time.time())])
 		if post_list[i]['caption'] == None: # check if there is caption
-			caption_list.append([post_list[i]['code'], ''])
+			caption_list.append([insta_id,post_list[i]['code'], '',int(time.time())])
 		else:
-			caption_list.append([post_list[i]['code'], post_list[i]['caption']['text'].replace("\n", " ")]) 
+			caption_list.append([insta_id,post_list[i]['code'], post_list[i]['caption']['text'].replace("\n", " "), int(time.time())]) 
 			# some captions have '\n' but we want them to be in one line
 
 	account_info = {'stats_list':stats_list, 'caption_list':caption_list}
@@ -82,10 +83,10 @@ def write_to_csv(list_name,file_name,col_names):
 		
 	output_file.close()
 
-def write_bio(insta_id):
+def write_account_bio(insta_id):
 	'''
 	INTAKE: an Instagram id
-	RETURN: a csv file with bio and total statistics
+	RETURN: a csv file with account bio
 	'''
 	r = requests.get("https://www.instagram.com/" + insta_id)
 	page_source = r.text
@@ -93,6 +94,19 @@ def write_bio(insta_id):
 	bio_start_index = page_source.find("<meta property=\"og:description\" content=") + len("<meta property=\"og:description\" content=")
 	bio_end_index = page_source.find("/>\n",bio_start_index)
 	bio = page_source[bio_start_index:bio_end_index]
+
+	bio_file = open(insta_id + '_account_bio.csv','w')
+	bio_writer = csv.writer(bio_file, encoding = 'utf-8')
+	bio_writer.writerow(['insta_id','bio','ts'])
+	bio_writer.writerow([insta_id,bio,int(time.time())])
+
+def write_account_stats(insta_id):
+	'''
+	INTAKE: an Instagram id
+	RETURN: a csv file with account bio
+	'''
+	r = requests.get("https://www.instagram.com/" + insta_id)
+	page_source = r.text
 
 	followers_start_index = page_source.find("\"followed_by\":") + len("\"followed_by\":")
 	followers_end_index = page_source.find("}",followers_start_index)
@@ -109,10 +123,10 @@ def write_bio(insta_id):
 	np = page_source[np_start_index:np_end_index]
 	np = "".join([s for s in np if s.isdigit()])
 
-	bio_file = open(insta_id + '_bio.csv','w')
+	bio_file = open(insta_id + '_account_stats.csv','w')
 	bio_writer = csv.writer(bio_file, encoding = 'utf-8')
-	bio_writer.writerow(['instagram_id','bio','num_of_posts','num_of_followers','num_of_followings'])
-	bio_writer.writerow([insta_id,bio,np,followers,follows])
+	bio_writer.writerow(['insta_id','num_of_posts','num_of_followers','num_of_followings','ts'])
+	bio_writer.writerow([insta_id,np,followers,follows,int(time.time())])
 
 
 if __name__ == '__main__':
@@ -121,7 +135,7 @@ if __name__ == '__main__':
 	post_list = get_post_list(insta_id)
 	account_info = extract_statistics(post_list)
 
-	write_to_csv(account_info['stats_list'],'stats_list',['post_id','num_of_likes','num_of_comments'])
-	write_to_csv(account_info['caption_list'],'caption_list',['post_id','caption'])
-	# download_photos(post_list)
-	write_bio(insta_id)
+	write_to_csv(account_info['stats_list'],'stats_list',['insta_id','post_id','num_of_likes','num_of_comments','ts'])
+	write_to_csv(account_info['caption_list'],'caption_list',['insta_id','post_id','caption','ts'])
+	write_account_bio(insta_id)
+	write_account_stats(insta_id)
